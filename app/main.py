11b6bdf96abb5bc99a85e699a8d054017ef2a58a
Matt Flowers <mattmflowers28@gmail.com>
2025-07-25 00:28:17 -0400

    Set up fastapi

---
+++ app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from enum import Enum
+from pydantic import BaseModel
+
+app = FastAPI(title="flowerpower")
+
+
+@app.get("/", include_in_schema=False)
+async def redirect_to_docs():
+    return RedirectResponse(url="/docs")
+
+
+class Status(str, Enum):
+    ok = "ok"
+
+
+class HealthResponse(BaseModel):
+    status: Status
+
+
+@app.get("/health")
+async def health_check() -> HealthResponse:
+    return HealthResponse(status=Status.ok)
